d0cf769d683bc8fdcf1d4d0196a554d5ee0bcbf1
fluffy <fluffy@beesbuzz.biz>
2017-07-04 21:12:21 -0700

    Add missing shaders

---
+++ src/shaders.lua
@@ -0,0 +1,13 @@
+--[[
+Refactor
+
+(c)2017 fluffy @ beesbuzz.biz. Please see the LICENSE file for license information.
+
+Some useful shaders
+]]
+
+local shaders = {
+    hueshift =  love.graphics.newShader("shaders/hueshift.fs")
+}
+
+return shaders
