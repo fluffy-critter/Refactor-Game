4e2ed76741fddbe5cbf86c3326e4e81b6e0cf8ae
fluffy <fluffy@beesbuzz.biz>
2017-07-09 18:02:47 -0700

    Fix shaders on Android (probably making them slightly faster anyway)

--- src/shaders/gaussBlur.fs
+++ src/shaders/gaussBlur.fs
@@ -7,15 +7,12 @@ references:
 
 uniform vec2 sampleRadius; // the radius for the kernel (1 pixel size in the filter direction)
 
-// borrowed from http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
-uniform float offset[3] = float[]( 0.0, 1.3846153846, 3.2307692308 );
-uniform float weight[3] = float[]( 0.2270270270, 0.3162162162, 0.0702702703 );
-
 vec4 effect(vec4 color, Image txt, vec2 tc, vec2 screen_coords) {
-    return (Texel(txt, tc))*weight[0]
-        + (Texel(txt, tc + offset[1]*sampleRadius))*weight[1]
-        + (Texel(txt, tc - offset[1]*sampleRadius))*weight[1]
-        + (Texel(txt, tc + offset[2]*sampleRadius))*weight[2]
-        + (Texel(txt, tc - offset[2]*sampleRadius))*weight[2];
+    // constants borrowed from http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
+    return (Texel(txt, tc))*0.2270270270
+        + (Texel(txt, tc + 1.3846153846*sampleRadius))*0.3162162162
+        + (Texel(txt, tc - 1.3846153846*sampleRadius))*0.3162162162
+        + (Texel(txt, tc + 3.2307692308*sampleRadius))*0.0702702703
+        + (Texel(txt, tc - 3.2307692308*sampleRadius))*0.0702702703;
 }
 
