8b472e9c73d86316d36069a541d6f8a57fa307d8
fluffy <fluffy@beesbuzz.biz>
2017-07-26 18:20:39 -0700

    Change RoamingEye's motion path to use smoothstep

--- src/geom.lua
+++ src/geom.lua
@@ -183,4 +183,9 @@ function geom.vectorLength(vector)
     return math.sqrt(vx*vx + vy*vy)
 end
 
+-- Implements the smoothStep equation from x=0..1
+function geom.smoothStep(x)
+    return x*x*(3 - 2*x)
+end
+
 return geom
