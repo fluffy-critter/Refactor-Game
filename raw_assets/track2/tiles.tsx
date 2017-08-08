1cfd2cce277f24bc922b1e742fc6fe310dac4b67
fluffy <fluffy@beesbuzz.biz>
2017-08-08 11:16:27 -0700

    More tweaks to the placeholder art that will eventually disappear wtf is wrong with me

--- raw_assets/track2/tiles.tsx
+++ raw_assets/track2/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="strangers" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
- <image source="../../src/track2/tiles.png" width="256" height="256"/>
+<tileset name="strangers" tilewidth="8" tileheight="8" tilecount="100" columns="10">
+ <image source="../../src/track2/tiles.png" width="80" height="80"/>
 </tileset>
