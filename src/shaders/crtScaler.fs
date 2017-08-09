d292c964981705d023e080c7a530c816fcf45527
fluffy <fluffy@beesbuzz.biz>
2017-08-08 22:04:03 -0700

    Tweak the CRT effect

--- src/shaders/crtScaler.fs
+++ src/shaders/crtScaler.fs
@@ -6,10 +6,10 @@
 uniform vec2 screenSize;
 
 vec4 effect(vec4 color, Image txt, vec2 tc, vec2 screen_coords) {
-    float phase = tc.x*screenSize.x*3.14159*2.0;
+    float phase = tc.x*screenSize.x*3.14159*2.2;
     vec4 scanColor = vec4(sin(phase)*0.15 + 0.85, sin(phase - 2.09)*0.15 + 0.85, sin(phase + 2.09)*0.15 + 0.85, 1.0);
 
     float row = tc.y*screenSize.y;
-    return color * vec4((Texel(txt, tc) * scanColor * sqrt(max(0.8, sin(row * 3.14159 * 2.0)))).rgb, 1.0);
+    return color * vec4((Texel(txt, tc) * scanColor * sqrt(max(0.8, sin(row * 3.14159 * 2.0)*1.25))).rgb, 1.0);
 }
 
