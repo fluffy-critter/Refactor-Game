8c1db4ba7d041770544ac21ed6b7fbaa93e75f99
fluffy <fluffy@beesbuzz.biz>
2017-12-13 01:00:08 -0800

    Naive attempt at getting actual timing

--- src/profiler.lua
+++ src/profiler.lua
@@ -14,40 +14,34 @@ local profiler = {}
 local font = love.graphics.newFont(16)
 
 local frameTime
-local contexts = {}
 local context
+local stats = {
+    counts = {},
+    total = 0
+}
+
+local lastTime
 
 local function hook()
     local info = debug.getinfo(2)
     if info then
-        local where = tostring(info.name) .. info.source .. ':' .. info.linedefined
-        context.counts[where] = (context.counts[where] or 0) + 1
-        context.total = context.total + 1
+        local where = context .. ':' .. tostring(info.name) .. info.source .. ':' .. info.linedefined
+        local now = love.timer.getTime()
+        stats.counts[where] = (stats.counts[where] or 0) + now - lastTime
+        stats.total = stats.total + now - lastTime
+        lastTime = now
     end
 end
 
-function profiler.attach(name, dt)
-    if dt then
-        frameTime = dt
-    end
+function profiler.attach(name)
+    context = name
 
-    if not contexts[name] then
-        contexts[name] = {
-            counts = {},
-            total = 0
-        }
-    end
-    context = contexts[name]
-
-    context.startTime = love.timer.getTime()
-
-    debug.sethook(hook, "", 100)
+    lastTime = love.timer.getTime()
+    debug.sethook(hook, "", 73)
 end
 
 function profiler.detach()
     debug.sethook()
-
-    context.totalTime = love.timer.getTime() - context.startTime
     context = nil
 end
 
@@ -59,34 +53,25 @@ function profiler.draw()
     love.graphics.push("all")
     love.graphics.origin()
     love.graphics.setBlendMode("alpha")
-    local y0 = 0
 
-    local activeTime = 0
-    for _,ctx in pairs(contexts) do
-        activeTime = activeTime + ctx.totalTime
+    local y = 0
+    local dy = love.graphics.getHeight()/stats.total
+    for k,count in util.spairs(stats.counts, function(t,a,b) return t[b] < t[a] end) do
+        local h = dy * count
+        love.graphics.rectangle("line", 0, y, 10, h)
+        if h > 8 then
+            love.graphics.setFont(font)
+            love.graphics.setColor(255,255,255)
+            love.graphics.print(k, 15, y)
+        end
+        y = y + h
     end
 
-    for _,ctx in pairs(contexts) do
-        local y = y0
-        local h0 = love.graphics.getHeight()*ctx.totalTime/activeTime
-        local dy = h0/ctx.total
-        for k,count in util.spairs(ctx.counts, function(t,a,b) return t[b] < t[a] end) do
-            local h = dy * count
-            love.graphics.rectangle("line", 0, y, 10, h)
-            if h > 8 then
-                love.graphics.setFont(font)
-                love.graphics.setColor(255,255,255)
-                love.graphics.print(k, 15, y)
-            end
-            y = y + h
-        end
-        y0 = y0 + h0
-
-        for k,v in pairs(ctx.counts) do
-            ctx.counts[k] = v*.9
-        end
-        ctx.total = ctx.total*.9
+    for k,v in pairs(stats.counts) do
+        stats.counts[k] = v*.9
     end
+    stats.total = stats.total*.9
+
     love.graphics.pop()
 end
 
