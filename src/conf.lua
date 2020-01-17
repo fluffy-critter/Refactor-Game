e596351699a4d917fb5dc71c86b71ca3657b93d7
fluffy <fluffy@beesbuzz.biz>
2020-01-16 22:10:45 -0800

    fix version in conf

--- src/conf.lua
+++ src/conf.lua
@@ -13,7 +13,7 @@ function love.conf(t)
     t.window.resizable = true
     t.window.fullscreen = false
 
-    t.version = "11.1"
+    t.version = "11.3"
 
     t.identity = "SockpuppetRefactor"
     t.window.title = "Sockpuppet - Refactor"
